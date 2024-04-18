81fce984dffb925542546cc81e0bca03776b1635
leizhenyu-lzy <lzy20190501@gmail.com>
2024-04-18 11:01:08 +0800

    move the whole folder from USB flash drive to PC

--- commandline.sh
+++ commandline.sh
@@ -2,6 +2,10 @@ echo "# Blog" > README.md
 
 echo "Record What I Learned" >> README.md
 
+echo "![](githubcat.gif)" >> README.md
+
+echo "**Table of Content**" >> README.md
+
 echo "\`\`\`text" >> README.md
 
 tree -d -L 2 >> README.md
