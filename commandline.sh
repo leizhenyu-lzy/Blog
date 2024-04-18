6200846c592cbbe88490c3aa9b690e7783f12350
leizhenyu-lzy <lzy20190501@gmail.com>
2024-04-18 11:02:07 +0800

    hello from PC

--- commandline.sh
+++ commandline.sh
@@ -1,16 +1,16 @@
-echo "# Blog" > README.md
+echo -e '# Blog\n\n---\n' > README.md
 
-echo "Record What I Learned" >> README.md
+echo -e 'Record What I Learned\n' >> README.md
 
-echo "![](githubcat.gif)" >> README.md
+echo -e '![](GithubCat.gif)\n\n---\n' >> README.md
 
-echo "**Table of Content**" >> README.md
+echo -e '**Table of Content**' >> README.md
 
-echo "\`\`\`text" >> README.md
+echo -e '```text' >> README.md
 
 tree -d -L 2 >> README.md
 
-echo "\`\`\`" >> README.md
+echo -e '```\n' >> README.md
 
 
 
