5b625e948e0b8845d6207758232d0347ac106f40
leizhenyu-lzy <lzy20190501@gmail.com>
2026-04-04 14:27:14 -0700

    add parallel joints

---
+++ Robotics/KinematicsKinetics/ParallelJoint.md
@@ -0,0 +1,8 @@
+# Parallel Joint - 并联关节
+
+
+参考文献 On the comprehensive kinematics analysis of a humanoid parallel ankle mechanism
+
+
+https://www.bilibili.com/video/BV1aE9sBRE9K
+
--- Robotics/KinematicsKinetics/test.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-print(2 * math.atan(120 / 135.21) / math.pi * 180)
-print(2 * math.atan(80 / 135.205) / math.pi * 180)
-print(2 * math.atan(40 / 60 * math.tan(math.radians(83.17864343711821 / 2))) / math.pi * 180)
